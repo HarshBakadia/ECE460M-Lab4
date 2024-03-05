b4ea56659f74087517a64c614297c5ac8010456e
Brandon King <47647479+BrandonKing770@users.noreply.github.com>
2024-03-05 17:22:04 -0600

    Update Counter_Interface.v

--- Srcs/Counter_Interface.v
+++ Srcs/Counter_Interface.v
@@ -41,57 +41,9 @@ initial begin
     Decr_Clk_Enable <= 1;
 end
 
-/*
-wire [7:0] Vector;
-parameter V_RESET    = 8'b00000001;
-parameter V_UP       = 8'b00000010;
-parameter V_LEFT     = 8'b00000100;
-parameter V_RIGHT    = 8'b00001000;
-parameter V_DOWN     = 8'b00010000;
-parameter V_SW0      = 8'b00100000;
-parameter V_SW1      = 8'b01000000;
-parameter V_Decr_Clk = 8'b10000000;
-
-assign Vector[0] = RESET;
-assign Vector[1] = UP;
-assign Vector[2] = LEFT;
-assign Vector[3] = RIGHT;
-assign Vector[4] = DOWN;
-assign Vector[5] = SW0;
-assign Vector[6] = SW1;
-assign Vector[7] = Decr_Clk && Decr_Clk_Enable;
-
-
-always@(Vector) begin
-    case(Vector)
-        V_RESET:  Out_Bin16 <= 0;
-        V_UP:     Out_Bin16 <= Out_Bin16 + 10;
-        V_LEFT:   Out_Bin16 <= Out_Bin16 + 180;
-        V_RIGHT:  Out_Bin16 <= Out_Bin16 + 200;
-        V_DOWN:   Out_Bin16 <= Out_Bin16 + 550;
-        V_SW0:    Out_Bin16 <= 10;
-        V_SW1:    Out_Bin16 <= 205;
-        
-        V_Decr_Clk: begin
-            if(Out_Bin16 > 0) Out_Bin16 <= Out_Bin16 - 1;
-            else Out_Bin16 <= 0;
-        end
-        
-        default: Out_Bin16 <= Out_Bin16;
-    endcase
-end
-
-always@(SW0, SW1)begin 
-    if(SW0 | SW1) Decr_Clk_Enable <= 0;
-    else Decr_Clk_Enable <= 1;
-end
-
-*/
 
 
 
-
-// STUPID UGLY SMASH
 always@(posedge RESET, posedge Up, posedge Left, posedge Right, posedge Down,
     posedge SW0, posedge SW1, posedge Decr_Clk) begin
     
